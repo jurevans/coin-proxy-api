4e4c35c7d09459cfbf3f41d5492aac20b04d2851
Justin R. Evans <jurevans@gmail.com>
2022-07-27 07:18:30 -0400

    Minor changes

--- server/api/gunicorn/config.py
+++ server/api/gunicorn/config.py
@@ -5,7 +5,6 @@ import os
 
 from distutils.util import strtobool
 
-
 bind = os.getenv('WEB_BIND', '0.0.0.0:8000')
 accesslog = '-'
 access_log_format = "%(h)s %(l)s %(u)s %(t)s '%(r)s' %(s)s %(b)s '%(f)s' '%(a)s' in %(D)sµs"  # noqa: E501
